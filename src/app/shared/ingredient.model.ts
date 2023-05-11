fe36b4f3bb7619b98c8a48eb811300839b0a3b5f
ufolx86 <victor.r.urueta@gmail.com>
2023-05-10 19:30:23 -0600

    Ingredient Model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+  // OTHER OPTION WITH ACCESOR FROM TYPESCRIPT SAME EFFECT AS BEFORE CREATES AND ASSIGN PROPERTIES
+  constructor(public name: string, public amount: number) {}
+}
