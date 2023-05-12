c97e2c5bcba5ca9521063ca64927a7bbaa5dbf8a
ufolx86 <victor.r.urueta@gmail.com>
2023-05-11 23:41:55 -0600

    Added different ways to pass props or events

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -16,20 +22,37 @@ export class CockpitComponent {
     serverName: string;
     serverContent: string;
   }>();
-  newServerName = '';
-  newServerContent = '';
+  // ORIGINAL VARIABLES FOR 2 WAY BINDING
+  // newServerName = '';
+  // newServerContent = '';
 
-  onAddServer() {
+  // USING NOW REFERENCES IMPORTANT JUST FOR READING
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
+    console.log(this.serverContentInput.nativeElement);
     this.serverCreatedEmitter.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      // serverName: this.newServerName, // USING OLD METHOD WITH 2 WAY BINDING
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreatedEmitter.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      // serverName: this.newServerName,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
+
+// @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
+// The same change (add { static: true } as a second argument) needs to be applied to ALL usages of @ViewChild()
+// (and also @ContentChild() which you'll learn about later) IF you plan on accessing the selected element inside of ngOnInit().
+
+// If you DON'T access the selected element in ngOnInit (but anywhere else in your component), set static: false instead!
+
+// If you're using Angular 9+, you only need to add { static: true } (if needed) but not { static: false }.
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  // encapsulation:ViewEncapsulation.Emulated //-> ANGULAR DEFAULT ISOLATED FOR COMPONENTS
+  // encapsulation:ViewEncapsulation.None //-> RESPECTS DEFAULT CSS (DISABLES ENCAPSULATION)
+  encapsulation: ViewEncapsulation.ShadowDom, //-> SAME AS EMULATED ONLY IN BROWSERS WHICH SUPPORT IT....DIG DEEPER
 })
 export class ServerElementComponent {
   // YOU CAN ASSIGN AN ALIAS TO THE INPUT, MAKE SURE IT IS UPDATED IN HTML IN THIS CASE srvElement is the alias for element
